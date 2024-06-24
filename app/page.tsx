1a663b6806a8160bbfd2f31647079cf974e0cdc2
Augustethegreat <chris@22onsloane.co>
2024-06-24 09:01:25 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+import Image from "next/image";
+import { useState } from "react";
+import { ChatBubbleBottomCenterTextIcon, HomeIcon, PhoneIcon, RectangleGroupIcon, WalletIcon, XMarkIcon } from "@heroicons/react/16/solid";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
+
+export default function Home() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [loginOpen, setLoginOpen] = useState(false);
+  return (
+
+    <div className="text-[#666b6a] hover:text-[#8aa19d]' bg-[#000000] ">
+      <div>
+        <p>Hello world</p>
+      </div>
+    </div>
+    
+
+    
+  
+  );
+}
